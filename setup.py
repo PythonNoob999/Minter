f4dca97018ad6347a03cbf795fbede485a01e357
SpicyPenguin <kerolisand@gmail.com>
2024-10-22 02:01:45 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@ setup(
     version="1.0.0",
     description="a lib/cli to mint nft's on any EVM chain (as long you fill the correct ABI args)",
     author="SpicyPenguin",
-    packages=["Minter"],
-    entry_points={
-        "console_scripts": [
-            "minter=Minter.cli:main"
-        ]
-    },
+    packages=["Minter", "Minter.types", "Minter.types.abis", "Minter.storage", "Minter.data"],
+    include_package_data=True,
+    package_data={"": ["*.json"]},
     install_requires=["kvsqlite", "web3"]
 )
